7cb625f62d6779a3acca9d2b9003fcb66cd50302
Yuvraj Singh Guleria <yuvrajguleria45@gmail.com>
2025-02-21 17:57:59 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import InputForm from "./components/InputForm";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>REST API Frontend</h1>
+      <InputForm />
+    </div>
+  );
+}
+
+export default App;
